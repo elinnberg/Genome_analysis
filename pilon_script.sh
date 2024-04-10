e9cfb15a239e92c9f8d5425e7363dac29a2f278c
Elin Berg <elinb@rackham3.uppmax.uu.se>
2024-04-10 13:43:52 +0200

    added the finalized pilon script

--- pilon_script.sh
+++ pilon_script.sh
@@ -3,14 +3,14 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:10:00
+#SBATCH -t 01:00:00
 #SBATCH -J elin_bwa_job
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user elinberg3@gmail.com
 #SBATCH --output=%x.%j.out
 
 #change or remove the folowing depending on when you run the script
-#SBATCH --reservation=uppmax2024-2-7_3
+
 
 # Load modules
 module load bioinfo-tools
@@ -18,4 +18,4 @@ module load Pilon/1.24
 
 # Your commands
 # Align Flye output to the BWA output (reference genome towards bwa_aligned.sorted.bam)
-java -jar pilon.jar --genome /home/elinb/Genome_analysis/assembly.fasta --bam bwa_aligned.sorted.bam --output /home/elinb/Genome_analysis/pilon_output
+java -jar $PILON_HOME/pilon.jar --genome /home/elinb/Genome_analysis/assembly.fasta --bam /home/elinb/Genome_analysis/bwa_alignments.sorted.bam --output /home/elinb/Genome_analysis/pilon_output
