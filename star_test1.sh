67f9d75ba99aa29c29f52fa7f7e4ff317f1b8b8e
Elin Berg <elinb@rackham3.uppmax.uu.se>
2024-04-16 16:34:23 +0200

    file with instructions for a single read in star

---
+++ star_test1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 8 #from manual
+#SBATCH -t 05:00:00
+#SBATCH -J elin_star2_job
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user elinberg3@gmail.com
+#SBATCH --output=%x.%j.out
+
+#change or remove the folowing depending on when you run the script
+#SBATCH --reservation=uppmax2024-2-7_5
+
+# Load modules
+module load bioinfo-tools
+module load star/2.7.11a
+
+# Set up STAR parameters-path to 16 files here, specifying scaffold 10
+#/home/elinb/Genome_Analysis/4_Tean_Teh_2017/transcriptome/trimmed/SRR6156069_scaffold_10.2.fastq.gz"
+
+# You run the STAR program with the forward and reverse strand for every sample in the trimmed directory. which will result in a new BAm file for every sample
+# Run STAR alignment
+STAR --genomeDir /home/elinb/Genome_analysis/genomeindex_4star \ # Path to the directory containing the indexed reference genome \
+     --readFilesIn /home/elinb/Genome_Analysis/Raw_Data/4_Tean_Teh_2017/transcriptome/trimmed/SRR6156069_scaffold_10.2.fastq.gz \
+     --runThreadN $SLURM_NTASKS \
+     --outSAMtype BAM SortedByCoordinate \
+     --outFileNamePrefix /home/elinb/Genome_analysis/star_results/mapped_reads_
+
