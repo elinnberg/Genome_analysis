9980c7c125ff0ed8c873abed8745c5574cabf4ab
Elin Berg <elinb@rackham1.uppmax.uu.se>
2024-05-02 15:04:59 +0200

    eggnog script that worked

--- eggnog_script.sh
+++ eggnog_script.sh
@@ -2,18 +2,24 @@
 #SBATCH -A uppmax2024-2-7
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 8 #from manual
-#SBATCH -t 05:00:00
+#SBATCH -n 2 #from manual
+#SBATCH -t 20:00:00
 #SBATCH -J eggnog_job
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user elinberg3@gmail.com
 #SBATCH --output=%x.%j.out
 
-#change or remove the folowing depending on when you run the script
-#SBATCH --reservation=uppmax2024-2-7_8
+#change or remove the folowing (reservation) depending on when you run the script
+
 
 module load bioinfo-tools
 module load eggNOG-mapper/2.1.9
+
 module load gffread/0.12.7
 
-gffread -E braker/Durian/GeneMark-ET/genemark.gtf -o genemark_gff/genemark.gff
+#genemark_durian is for my annotation that I did with the correct script.
+gffread -E braker/Durian/augustus.hints.gff -o genemark_gff/genemark_durian.gff
+
+# Use eggnogmapper for my fasta file i used for braker, as well as the genemark file.
+# emapper.py -m diamond -i pilon_output.fasta.masked -i genemark_gff/genemark_durian.gff 
+emapper.py -m diamond --itype CDS --translate -i pilon_output.fasta.masked -o eggnogresult --decorate_gff genemark_gff/genemark_durian.gff
